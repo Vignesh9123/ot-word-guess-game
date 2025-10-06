4647408d13a7f934cf824a7b613c906e629b3b66
Vignesh D <vignesh.d9123@gmail.com>
2025-10-06 12:34:41 +0530

    Update init script to add admin user

--- wordgame/management/commands/initdb.py
+++ wordgame/management/commands/initdb.py
@@ -1,14 +1,20 @@
+from django.contrib.auth.hashers import make_password
 from django.core.management.base import BaseCommand
-from wordgame.models import Word
+from wordgame.models import Word, User
 
 class Command(BaseCommand):
     help = "Populate the database with initial data"
 
     def handle(self, *args, **options):
-        words = ["AUDIO","HOMER","JOKER","TONER","TOWER","APPLE","GRAPE","PLANT","SHIFT","BRAVE",
-                "CRANE","DRIVE","EPOCH","FAITH","GHOST","HONEY","INNER","JUDGE","KNOCK","LIGHT"]
-        for i in range(20):
-            word = Word(text=words[i])
-            word.save() 
+        if not Word.objects.exists():
+            words = ["AUDIO","HOMER","JOKER","TONER","TOWER","APPLE","GRAPE","PLANT","SHIFT","BRAVE",
+                    "CRANE","DRIVE","EPOCH","FAITH","GHOST","HONEY","INNER","JUDGE","KNOCK","LIGHT"]
+            for i in range(20):
+                word = Word(text=words[i])
+                word.save() 
         
-        self.stdout.write(self.style.SUCCESS("Database initialized successfully."))
+        if not User.objects.filter(username="AdminUser").exists():
+            user = User(username="AdminUser", password=make_password("AdminPassword@123"), is_admin=True)
+            user.save()
+        
+        self.stdout.write(self.style.SUCCESS("Database initialized successfully with admin user. Please login with username: AdminUser and password: AdminPassword@123"))
